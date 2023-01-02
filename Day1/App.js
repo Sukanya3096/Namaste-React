eef4ce163d90c3061cc5eff1616de206df23263c
Sukanya3096 <isukanyadutta@gmail.com>
2023-01-02 15:57:44 +0530

    day 1 assignment

---
+++ Day1/App.js
@@ -0,0 +1,8 @@
+const heading = React.createElement("h1", { id: "title1" }, "Heading 1");
+const heading2 = React.createElement("h1", { id: "title2" }, "Heading 2");
+const container = React.createElement("div", { id: "container" }, [
+  heading,
+  heading2,
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
