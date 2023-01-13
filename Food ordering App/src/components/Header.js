767276655087f1a6bff5c90f217a736677c8db6c
Sukanya3096 <isukanyadutta@gmail.com>
2023-01-13 22:12:23 +0530

    added geolocation

--- Food ordering App/src/components/Header.js
+++ Food ordering App/src/components/Header.js
@@ -6,20 +6,71 @@ import {
   ShoppingCartRounded,
 } from "@mui/icons-material";
 import IconButton from "@mui/material/IconButton";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import restuarant from "../../../Food ordering App/images/restuarant.png";
+import { GOOGLE_API_KEY } from "../constants";
+import Geocode from "react-geocode";
 
 const Header = (props) => {
   const [cart, setCart] = useState();
   const [searchText, setSearchText] = useState("");
+  const [address, setAddress] = useState("");
 
   const searchHandler = (text) => {
     setSearchText(text.target.value);
     props.onSearch(text.target.value);
   };
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      codeLatLng(position.coords.latitude, position.coords.longitude);
+    });
+  }, []);
+
+  function codeLatLng(lat, lng) {
+    Geocode.setApiKey(GOOGLE_API_KEY); // USE YOUR GOOGLE API KEY!
+
+    Geocode.fromLatLng(lat, lng).then(
+      (response) => {
+        let city, state, country, zip;
+        for (
+          let i = 0;
+          i < response.results[0].address_components.length;
+          i++
+        ) {
+          for (
+            let j = 0;
+            j < response.results[0].address_components[i].types.length;
+            j++
+          ) {
+            switch (response.results[0].address_components[i].types[j]) {
+              case "locality":
+                city = response.results[0].address_components[i].long_name;
+                break;
+              case "administrative_area_level_1":
+                state = response.results[0].address_components[i].long_name;
+                break;
+              case "country":
+                country = response.results[0].address_components[i].long_name;
+                break;
+              case "postal_code":
+                zip = response.results[0].address_components[i].long_name;
+                break;
+            }
+          }
+        }
+        setAddress(`${city}, ${state} ${zip}, ${country}`);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+
   return (
     <header>
       <img src={restuarant} alt="" className="logo" />
+      <span className="address">{address}</span>
 
       <div className="inputBox">
         <SearchRounded className="searchIcon" />
--- Food ordering App/src/constants.js
+++ Food ordering App/src/constants.js
@@ -1,6 +1,8 @@
 export const IMG_CDN_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
+export const GOOGLE_API_KEY = ""; // YOUR GOOGLE API KEY
+
 export const restaurantList = [
   {
     type: "restaurant",
